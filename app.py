c2a1ddc70d0b6ae0806fc1c2694bdc659c3ca7cf
MangoDingo33 <mangodingo33@gmail.com>
2025-07-18 09:07:24 +0900

    '7'

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
+plt.rcParams['font.family'] ='Malgun Gothic'
+plt.rcParams['axes.unicode_minus'] =False
     
 @st.cache_data
 def get_filming_location_list() -> pd.DataFrame:
